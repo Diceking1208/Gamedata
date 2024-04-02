ecec9345de6f62f8558dc1a3a9c0dddaef3e4e39
sjgoodgood <ehtntjf35@naver.com>
2024-04-02 19:13:58 +0900

    0402 Re제로부터 시작하는 노숙자의 생활수정

---
+++ Re제로부터 시작하는 노숙자 생활2/Re제로부터 시작하는 노숙자 생활2/name.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+#define MAX_PROBABILITY 100.0f              // 성공확률 최대치
+
+int main(void)
+{
+    int isTry = 0;                          // 강화를 할 것인지 선택
+    int level = 0;                          // 현재 무기의 레벨
+    time_t retTime = 0;                     // 대기시간 임시저장 변수
+    int randNum = 0;                        // 랜덤값을 저장할 변수
+    int Num = 100;                          // 확률 숫자
+    int k = 0;                              //집 구매
+    int m = 0;                                //집 팔때
+    int a = 500000;                         //총 금액
+
+    char* house[] = { "서울역 노숙","반지하원룸","벌레 나오는 원룸", "좁은 원룸","적당한 원룸","넓은 원룸","벌레 가끔 나오는 투룸","층간소음 지리는 투룸","변기 잘 막히는 투룸","적당한 투룸","쩌는 투룸","걍 아파트","그냥 좋은 아파트","멋있는 아파트","호화로운 아파트","마당있는 단독주택","시그니엘" };
+    srand((int)time(NULL));                 // 랜덤 시드값 설정
+
+    while (1)
+    {
+
+        // 화면 정리
+        system("@cls||clear");
+
+        printf("성공확률 : %d %%\n", Num);
+        printf("집을 구매하시겠습니까?\n");
+        printf("1.집 구매   2.거지로 살기 \n");
+
+        switch (level)
+        {
+        case 0: Num = 100; k = 2000; m = 0;  break;
+        case 1: Num = 95; k = 4000; m = 10000; break;
+        case 2: Num = 90; k = 6000; m = 20000; break;
+        case 3: Num = 87; k = 8000; m = 25000; break;
+        case 4: Num = 85;  k = 10000; m = 30000; break;
+        case 5: Num = 83; k = 12000; m = 35000; break;
+        case 6: Num = 80; k = 14000; m = 40000; break;
+        case 7: Num = 77;  k = 16000; m = 45000; break;
+        case 8: Num = 75; k = 18000; m = 50000; break;
+        case 9: Num = 73; k = 20000; m = 55000; break;
+        case 10: Num = 70; k = 22000; m = 60000;  break;
+        case 11: Num = 68; k = 24000; m = 65000; break;
+        case 12: Num = 65; k = 26000; m = 70000; break;
+        case 13: Num = 63; k = 28000; m = 75000; break;
+        case 14: Num = 60; k = 30000; m = 80000; break;
+        case 15: Num = 58; k = 33000; m = 85000; break;
+        case 16: Num = 55; k = 36000; m = 90000; break;
+        default:
+            break;
+        }
+
+
+
+        // 화면 정리
+        system("@cls||clear");
+        printf("총 금액 : %d\n", a);
+        printf("  현재 집 : %s\n", house[level]);
+        printf("     성공확률 : %d %%\n", Num);
+        printf(" 집을 강화 하시겠습니까 ? \n");
+        printf("1.집 구매   2.거지로 살기 3.집팔기\n");
+        printf("---------------------------\n");
+        printf("        입력 : ");
+        scanf_s("%d", &isTry);
+
+        switch (isTry)
+        {
+        case 1:        // 강화에 도전 할 경우
+            randNum = rand() % 100 + 1;
+
+            // 추출한 랜덤 값이 성공확률 보다 작으면 성공
+            if (randNum < Num) {
+                // 성공화면 출력
+                a -= k;
+
+                printf("\n\n************* SUCCESS *************\n\n");
+                printf("  %s  ->  %s    \n\n", house[level], house[level + 1]);
+                printf("************* SUCCESS *************\n");
+                printf("현재금액 : %d", a);
+                level++;
+
+
+
+            }
+            else
+            {
+                // 실패화면 출력
+                a -= k;
+                printf("\n\n아 망했네..\n");
+                printf("%s ...  집을 잃었습니다.\n", house[0]);
+
+                // 각종 수치 초기화
+                k = 50000;
+                level = 0;
+            }
+            break;
+
+        case 2:
+            // 포기를 할 경우 프로그램 종료
+            printf("어 나가~\n");
+            printf("\n         어 나가~\n");
+            return -1;
+
+        case 3:
+            a += +m;
+            printf("  %s  ->  %s    \n\n", house[level], house[level - 1]);
+            printf("현재금액 : %d", a);
+            level--;
+
+        }
+        // 진행상황 확인이 용이 하도록 대기
+        printf("\n계속하려면 아무 키나 누르십시오.\n");
+        _getch();
+    }
+
+    return 0;
+}
